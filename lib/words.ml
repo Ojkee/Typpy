2089086c2f58504398fc9f33f685996f7820be84
Ojkee <17oju17@gmail.com>
2025-07-12 18:37:02 +0200

    words update

--- lib/words.ml
+++ lib/words.ml
@@ -1,4 +1,4 @@
-(* TODO : open Base*)
+open Base
 
 type t = string array
 
@@ -8,14 +8,16 @@ let acceptable_word min max = function
       min <= len && len <= max
 
 let file_content file_name =
-  let ic = open_in file_name in
+  let ic = Stdio.In_channel.create file_name in
   let content = In_channel.input_all ic in
-  close_in ic;
+  Stdio.In_channel.close ic;
   content
 
 let create ~file_name ~min ~max =
-  file_content file_name |> String.split_on_char '\n' |> List.map String.trim
-  |> List.filter (acceptable_word min max)
+  file_content file_name
+  |> String.split_on_chars ~on:[ '\n' ]
+  |> List.map ~f:String.strip
+  |> List.filter ~f:(acceptable_word min max)
   |> Array.of_list
 
 let length words = Array.length words
